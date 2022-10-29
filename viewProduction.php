cf750aa60f88eedcc622940667379133cb6286a8
unknown <agustuslam12@gmail.com>
2022-10-29 12:55:51 +0700

    first commit

---
+++ viewProduction.php
@@ -0,0 +1,43 @@
+<link rel="stylesheet" href="./style.css">
+
+<div >
+  <h2>Production</h2>
+  <table class="table ">
+    <thead>
+      <tr>
+        <th class="text-center">No</th>
+        <th class="text-center">Plane</th>
+        <th class="text-center">Actual</th>
+        <th class="text-center">Balance</th>
+        <th class="text-center">status</th>
+        <th class="text-center" colspan="1">Action</th>
+      </tr>
+    </thead>
+    
+    <tr>
+      <td>1</td>
+      <td>242</td>
+     
+      <td>-49</td> 
+      <td>29.12%</td>      
+      <td>break time</td>      
+      <td><button class="btn btn-primary" style="height:40px" onclick="variationEditForm('')">view</button></td>
+      </tr>
+      
+  </table>
+
+   <!-- Trigger the modal with a button -->
+   <button type="button" class="btn btn-secondary" style="height:40px" data-toggle="modal" data-target="#myModal">
+    Add Production
+  </button>
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      
+    </div>
+  </div>
+  
+</div>
+   
